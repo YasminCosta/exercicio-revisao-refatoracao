54943db76cd21b2e1803dec8a2725d092bb3b6ff
YasminCosta <49276233+YasminCosta@users.noreply.github.com>
2020-10-20 23:02:51 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,20 @@
 #include <string>
 #include "Empregado.hpp"
+#include <iostream>
 
 class Vendedor : public Empregado {
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
+  public:	
+			double quotaMensalVendas;
+
+			double quotaTotalAnual() {
+			return quotaMensalVendas * 12;
+		}
+	void saidaVendedor (double horasTrabalhadas){
+    this->saidaEmpregado(horasTrabalhadas);
+		std::cout << "Quota vendas: " << this->quotaTotalAnual() << std::endl;
+  	std::cout << std::endl;
 	}
-	
+
 };
 
