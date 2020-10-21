8924061599a88785190932a48aec55c9e0a97db1
YasminCosta <49276233+YasminCosta@users.noreply.github.com>
2020-10-20 23:03:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,59 +6,45 @@ int main() {
   Engenheiro eng1;
   eng1.nome = "Joao Snow";
   eng1.salarioHora = 35;
-  eng1.projetos = 3; 
-  std::cout << "Nome: " << eng1.nome << std::endl;
-  std::cout << "Salario Mes: " << eng1.pagamentoMes(9.5) << std::endl;
-  std::cout << "Projetos: " << eng1.projetos << std::endl;
-  std::cout << std::endl;
+  eng1.projetos = 3;
+  //Parâmetro é quantidade de horas trabalhadas 
+  eng1.saidaEngenheiro(9.5);
   
   Engenheiro eng2;
   eng2.nome = "Daniela Targaryen";
   eng2.salarioHora = 30;
-  eng2.projetos = 1; 
-  std::cout << "Nome: " << eng2.nome << std::endl;
-  std::cout << "Salario Mes: " << eng2.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng2.projetos << std::endl;  
-  std::cout << std::endl;
+  eng2.projetos = 1;
+  //Parâmetro é quantidade de horas trabalhadas 
+  eng2.saidaEngenheiro(8);
   
   Engenheiro eng3;
   eng3.nome = "Bruno Stark";
   eng3.salarioHora = 30;
-  eng3.projetos = 2; 
-  std::cout << "Nome: " << eng3.nome << std::endl;
-  std::cout << "Salario Mes: " << eng3.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng3.projetos << std::endl;  
-  std::cout << std::endl;
+  eng3.projetos = 2;
+  //Parâmetro é quantidade de horas trabalhadas
+  eng3.saidaEngenheiro(8);
   
   
   Vendedor vend1;
   vend1.nome = "Tonho Lannister";
   vend1.salarioHora = 20;
   vend1.quotaMensalVendas = 5000;
-  
-  std::cout << "Nome: " << vend1.nome << std::endl;
-  std::cout << "Salario Mes: " << vend1.pagamentoMes(6) << std::endl;  
-  std::cout << "Quota vendas: " << vend1.quotaTotalAnual() << std::endl;
-  std::cout << std::endl;
+  //Parâmetro é quantidade de horas trabalhadas
+  vend1.saidaVendedor(6);  
   
   Vendedor vend2;
   vend2.nome = "Jose Mormont";
   vend2.salarioHora = 25;
   vend2.quotaMensalVendas = 3000;
-  
-  std::cout << "Nome: " << vend2.nome << std::endl;
-  std::cout << "Salario Mes: " << vend2.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend2.quotaTotalAnual() << std::endl; 
-  std::cout << std::endl;  
+  //Parâmetro é quantidade de horas trabalhadas
+  vend2.saidaVendedor(8);  
 	
   Vendedor vend3;
   vend3.nome = "Sonia Stark";
   vend3.salarioHora = 30;
   vend3.quotaMensalVendas = 4000;
-  
-  std::cout << "Nome: " << vend3.nome << std::endl;
-  std::cout << "Salario Mes: " << vend3.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend3.quotaTotalAnual() << std::endl;  
+  //Parâmetro é quantidade de horas trabalhadas
+  vend3.saidaVendedor(8);
   
   return 0;	
 }
